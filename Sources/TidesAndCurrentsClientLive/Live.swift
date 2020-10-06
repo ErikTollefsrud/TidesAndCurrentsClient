12d8f212d6753090bc0917c7ba28b9d10cea2e42
Erik Tollefsrud <erik.tollefsrud@gmail.com>
2020-10-06 16:21:18 -0500

    Old live url was getting old data, not predictions. Update url now gets next 48 hours of tide predictions

--- Sources/TidesAndCurrentsClientLive/Live.swift
+++ Sources/TidesAndCurrentsClientLive/Live.swift
@@ -12,23 +12,28 @@ import TidesAndCurrentsClient
 extension TidesClient {
     public static let live = Self(
         stations: {
-        let url = URL(string: "https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.json?type=tidepredictions")!
-        
-        return URLSession.shared.dataTaskPublisher(for: url)
-            //.print()
-            .map { data, _ in data }
-            .decode(type: StationLocations.self, decoder: JSONDecoder())
-            .mapError { _ in Failure() }
-            .map { $0.stations }
-            .eraseToAnyPublisher()
-        }, tidePredictionData: { stationId in
-        let url = URL(string: "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&range=24&datum=MLLW&station=\(stationId)&time_zone=LST&units=english&interval=hilo&format=json)")!
-        
-        return URLSession.shared.dataTaskPublisher(for: url)
-            .map { data, _ in data }
-            .decode(type: TidePredictions.self, decoder: JSONDecoder())
-            .mapError { _ in Failure() }
-            .map { $0 }
-            .eraseToAnyPublisher()
-    })
+            let url = URL(string: "https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.json?type=tidepredictions")!
+            
+            return URLSession.shared.dataTaskPublisher(for: url)
+                .map { data, _ in data }
+                .decode(type: StationLocations.self, decoder: JSONDecoder())
+                .mapError { _ in Failure() }
+                .map { $0.stations }
+                .eraseToAnyPublisher()
+        },
+        tidePredictionData: { stationId in
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyyMMdd"
+            let nowString = formatter.string(from: Date())
+            
+            let url = URL(string: "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&begin_date=\(nowString)&range=48&datum=MLLW&station=\(stationId)&time_zone=LST_LTD&units=english&interval=hilo&format=json)")!
+            
+            return URLSession.shared.dataTaskPublisher(for: url)
+                .map { data, _ in data }
+                .decode(type: TidePredictions.self, decoder: JSONDecoder())
+                .mapError { _ in Failure() }
+                .map { $0 }
+                .eraseToAnyPublisher()
+        })
 }
